7b677967490e9f5532cef7b244df49c386b082cc
RaphaelAN <raphael.novello@gmail.com>
2021-10-11 17:41:20 -0300

    Adicionando queries de campus e curso

--- routes/route_example.js
+++ routes/route_example.js
@@ -22,6 +22,43 @@ router.get("/bd_ufv", (req, res, next) => {
         })
 });
 
+router.get("/bd_ufv/:campus", (req, res, next) => {
+    db.raw(`SELECT Curso.nome, SUM(Turma.NumEstudantes) , SUM(Turma.Aprovados) 
+            FROM Campus 
+            LEFT JOIN Curso 
+            ON Campus.SiglaCamp = Curso.SiglaCamp
+            LEFT JOIN Turma
+            ON Turma.CodCurso = Curso.CodCurso
+            WHERE Campus.nome = ${req.params.campus}
+            GROUP BY Curso.CodCurso`)
+        .then((data) => {
+            res.send(data[0]);
+        }).catch(err => {
+            console.log(err)
+            res.send(err);
+        })
+});
+
+router.get("/bd_ufv/:campus/:curso", (req, res, next) => {
+    db.raw(`SELECT Disciplina.Nome, SUM(Turma.NumEstudantes), SUM(Turma.Aprovados) 
+            FROM Campus 
+            LEFT JOIN Curso 
+            ON Campus.SiglaCamp = Curso.SiglaCamp
+            LEFT JOIN Turma
+            ON Turma.CodCurso = Curso.CodCurso
+            LEFT JOIN Disciplina
+            ON Turma.CodDisc = Disciplina.CodDisc
+            WHERE Campus.nome = ${req.params.campus} 
+            AND Curso.nome = ${req.params.curso}
+            GROUP BY Turma.CodDisc, Disciplina.Nome`)
+        .then((data) => {
+            res.send(data[0]);
+        }).catch(err => {
+            console.log(err)
+            res.send(err);
+        })
+});
+
 // router.get("/:id", (req, res, next) => {
 //     res.status(200).send(`Hello! Parameter: ${req.params.id}`);
 // });
