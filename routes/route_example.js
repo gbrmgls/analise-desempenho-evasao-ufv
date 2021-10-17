9556a40f2b19d2b64913dcc880db032616d06f18
unknown <joao_ps@dcc.ufrj.br>
2021-10-17 16:43:55 -0300

    Rota para consulta de disciplinas para cada departamento e para cada ano

--- routes/route_example.js
+++ routes/route_example.js
@@ -22,6 +22,61 @@ router.get("/bd_ufv", (req, res, next) => {
         })
 });
 
+router.get("/bd_ufv/departamento/:depto", (req, res, next) => {
+    db.raw(`SELECT Disciplina.CodDisc, Disciplina.Nome, sum(Turma.NumEstudantes), sum(Turma.Aprovados) 
+            FROM Departamento
+            JOIN Disciplina
+            ON Departamento.SiglaDepto = Disciplina.SiglaDepto
+            JOIN Turma
+            ON Turma.CodDisc = Disciplina.CodDisc
+            WHERE Departamento.SiglaDepto = '${req.params.depto}'
+            GROUP BY Disciplina.CodDisc, Disciplina.Nome
+            ORDER BY sum(Turma.Aprovados)/sum(Turma.NumEstudantes)`)
+        .then((data) => {
+            res.send(data);
+        }).catch(err => {
+            console.log(err)
+            res.send(err);
+        })
+});
+
+router.get("/bd_ufv/departamento/:depto/:ano", (req, res, next) => {
+    db.raw(`SELECT Disciplina.CodDisc, Disciplina.Nome, sum(Turma.NumEstudantes), sum(Turma.Aprovados) 
+            FROM Departamento
+            JOIN Disciplina
+            ON Departamento.SiglaDepto = Disciplina.SiglaDepto
+            JOIN Turma
+            ON Turma.CodDisc = Disciplina.CodDisc
+            WHERE Departamento.SiglaDepto = '${req.params.depto}'
+            AND Turma.Ano = ${req.params.ano}
+            AND Turma.Ano = Disciplina.Ano
+            GROUP BY Disciplina.CodDisc, Disciplina.Nome
+            ORDER BY sum(Turma.Aprovados)/sum(Turma.NumEstudantes)`)
+        .then((data) => {
+            res.send(data);
+        }).catch(err => {
+            console.log(err)
+            res.send(err);
+        })
+});
+
+router.get("/bd_ufv/:campus", (req, res, next) => {
+    db.raw(`SELECT Curso.nome, SUM(Turma.NumEstudantes) , SUM(Turma.Aprovados) 
+            FROM Campus 
+            LEFT JOIN Curso 
+            ON Campus.SiglaCamp = Curso.SiglaCamp
+            LEFT JOIN Turma
+            ON Turma.CodCurso = Curso.CodCurso
+            WHERE Campus.SiglaCamp = "${req.params.campus}"
+            GROUP BY Curso.CodCurso`)
+        .then((data) => {
+            res.send(data[0]);
+        }).catch(err => {
+            console.log(err)
+            res.send(err);
+        })
+});
+
 router.get("/bd_ufv/:campus", (req, res, next) => {
     db.raw(`SELECT Curso.nome, SUM(Turma.NumEstudantes) , SUM(Turma.Aprovados) 
             FROM Campus 
